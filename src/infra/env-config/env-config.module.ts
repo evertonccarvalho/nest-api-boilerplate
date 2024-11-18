2046ffa2a24f21714f8891e8429f5bb78498efd3
Éverton Carvalho <evertonsnake@gmail.com>
2024-11-17 23:01:50 -0300

    jest config

--- src/infra/env-config/env-config.module.ts
+++ src/infra/env-config/env-config.module.ts
@@ -1,6 +1,5 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
-import { join } from 'node:path';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EnvConfigService } from './env-config.service';
 import appConfig from './env.config';
 @Module({
@@ -8,11 +7,4 @@ import appConfig from './env.config';
   providers: [EnvConfigService],
   exports: [EnvConfigService],
 })
-export class EnvConfigModule extends ConfigModule {
-  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
-    return super.forRoot({
-      ...options,
-      envFilePath: [join(__dirname, `../../../.env.${process.env.NODE_ENV}`)],
-    });
-  }
-}
+export class EnvConfigModule extends ConfigModule {}
